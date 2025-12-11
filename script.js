a761b0bfa2d686bd796c0c362516552b70822375
Matias29S <130400263+Matias29S@users.noreply.github.com>
2025-12-11 14:20:51 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const navItems = document.querySelectorAll('.encabezado a');
+    const navItems = document.querySelectorAll('.encabezado a'); /*selecciona todas las clases "encabezado a"*/
 
     navItems.forEach(item => {
-        item.addEventListener('mouseover', () => {
+        item.addEventListener('mouseover', () => { /*un evento donde al pasar el mouse por el boton le agrega la clase hover-js*/
             item.classList.add('hover-js');
         });
 
-        item.addEventListener('mouseout', () => {
+        item.addEventListener('mouseout', () => { /*ahora al quitarlo se lo quita*/
             item.classList.remove('hover-js');
         });
     });
+
 });
